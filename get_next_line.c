d2c55cfd1019d850559d293742742fffdc3f2b7d
Radek Novotny <rnovotny@c2r8s3.42prague.com>
2023-01-23 22:28:13 +0100

    First commit

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 20:28:35 by rnovotny          #+#    #+#             */
+/*   Updated: 2023/01/23 21:43:50 by rnovotny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+TODO: make buffer-size independent - 
+	- read char by char until \n or end of file is encountered
+TODO: first read line to find it's length, 
+	then malloc and read again, saving into malloced string 
+*/
+
+char *get_next_line(int fd)
+{
+	char *buffer;
+	
+
+	return (buffer);
+}
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 20:29:14 by rnovotny          #+#    #+#             */
+/*   Updated: 2023/01/23 20:58:30 by rnovotny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
